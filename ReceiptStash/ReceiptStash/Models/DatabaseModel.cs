f4c7e05e79896a0c47b9ccdf7a1652d27537a8b1
Jiahua Zhang <jzhang113@uiowa.edu>
2017-09-16 14:06:59 -0500

    created new project

---
+++ ReceiptStash/ReceiptStash/Models/DatabaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ReceiptStash.Models
+{
+    public class DatabaseModel : IDatabaseModel
+    {
+        private static SqlConnection _conn;
+
+        static DatabaseModel()
+        {
+            _conn = new SqlConnection("Data Source=(local);Integrated Security=SSPI");
+        }
+    }
+}
